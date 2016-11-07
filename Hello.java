7b6d0f190473f0b4c79fd6bd9e0288f4853015e2
knromaric <nzekeng@gmail.com>
2016-11-07 19:44:12 +0200

    out inserted

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
 	public static void main(String[] args){
-		for(int i=1; i<=100; i++){
-			System..println("Hello Satelliz... "+i);
+		for(int i=1; i<=500; i++){
+			System.out.println("Hello Satelliz... "+i);
 		}
 	}
 }
